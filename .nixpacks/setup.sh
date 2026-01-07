4a8bff87af43bd6b2e7c94f38231fe8dbade4a23
District Interiors <dev@district.sa>
2026-01-07 17:08:12 +0500

    Fix: Create symlink for pnpm in /usr/local/bin

---
+++ .nixpacks/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+npm install -g pnpm@latest
+export PATH="$PATH:$(npm config get prefix)/bin"
+echo "export PATH=\"\$PATH:$(npm config get prefix)/bin\"" >> /root/.bashrc
+echo "export PATH=\"\$PATH:$(npm config get prefix)/bin\"" >> /root/.profile
+
